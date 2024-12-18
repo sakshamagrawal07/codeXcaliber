63ad558cda6430ebf96b5719325f42e31d922d3a
Saksham Agrawal <agsaksham07@gmail.com>
2024-12-18 13:00:23 +0530

    projects and faq added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import FAQSection from './components/FAQ/FAQ';
 import Timeline from './components/Timeline/Timeline'
 import Footer from './components/footer/footer';
 import Hero from "./components/hero/hero";
 import Navbar from "./components/navbar/navbar";
+import Projects from './components/projects/projects';
 
 
 export default function App() {
@@ -11,6 +13,8 @@ export default function App() {
       <Navbar/>
       <Hero/>
       <Timeline/>
+      <Projects/>
+      <FAQSection/>
       <Footer/>
     </>
   );
