d7219c94d9a1b315ad67d682cadce173c9290c67
xcurx <suj5al78@gmail.com>
2025-01-03 21:40:35 +0530

    update timeline dates

--- src/components/Carousel/CarouselReplacement.jsx
+++ src/components/Carousel/CarouselReplacement.jsx
@@ -2,7 +2,7 @@
 const CarouselReplacement = () => {
   return (
     <div>
-        <h1 className="text-4xl mt-2 font-bold">Sponsors</h1>
+        <h1 className="md:text-4xl text-3xl mt-2 font-bold">Sponsors</h1>
         <p className="my-4 text-[#aaa]">To be announced soon</p>
     </div>
   )
--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -27,45 +27,45 @@ const useWidth = () => {
 const events = [
   {
     id: "01",
-    date: "15.11.2024",
-    title: "STUDENT APPLICATIONS",
-    description: "This marks the period when students can submit their applications to participate in Winter of Code. Ensure your application is complete before the deadline!"
+    date: "15.01.2025",
+    title: "REGISTRATION BEGINS",
+    description: "This marks the period when students can submit their applications to participate in CodeXCaliber. Ensure your application is complete before the deadline!"
   },
   {
     id: "02",
-    date: "20.12.2024",
+    date: "24.01.2025",
     title: "INAUGURAL CEREMONY",
-    description: "The official start of Winter of Code! Join us for an introductory session where we’ll walk you through the program’s timeline, expectations, and the exciting journey ahead."
+    description: "The official start of CodeXCaliber! Join us for an introductory session where we’ll walk you through the program’s timeline, expectations, and the exciting journey ahead."
   },
   {
     id: "03",
-    date: "05.01.2025",
+    date: "25.01.2025",
     title: "PROPOSAL WRITING",
     description: "A week-long workshop focused on helping students write strong, well-structured project proposals. Get guidance from mentors and learn the best practices for proposing your project ideas."
   },
   {
     id: "04",
-    date: "12.01.2025",
+    date: "25.01.2025",
     title: "COMMUNITY BONDING PHASE",
     description: "This phase encourages students and mentors to connect, discuss their projects, and build rapport with the organisations. It’s a great opportunity to understand your project’s goals better before diving into coding."
   },
   {
     id: "05",
-    date: "12.01.2025",
+    date: "15.02.2025",
     title: "CODINGPHASE",
-    description: "This is where the real fun begins! The core phase of Winter of Code, where students begin implementing their proposed projects. During this period, students will collaborate closely with mentors and contribute code to open-source repositories."
+    description: "This is where the real fun begins! The core phase of CodeXCaliber, where students begin implementing their proposed projects. During this period, students will collaborate closely with mentors and contribute code to open-source repositories."
   },
   {
     id: "06",
-    date: "12.01.2025",
-    title: "MENTOR EVALUATION",
+    date: "03.04.2025",
+    title: "MENTOR EVALUATION PHASE",
     description: "Mentors will review the progress of students’ projects, providing feedback and guidance. This evaluation is crucial for refining and improving the final submission."
   },
   {
     id: "07",
-    date: "12.01.2025",
+    date: "07.04.2025",
     title: "CLOSING CEREMONY",
-    description: "The official conclusion of Winter of Code. Celebrate your achievements, review key learnings, and recognize outstanding contributors as we wrap up the program."
+    description: "The official conclusion of CodeXCaliber. Celebrate your achievements, review key learnings, and recognize outstanding contributors as we wrap up the program."
   },
 ]
 
