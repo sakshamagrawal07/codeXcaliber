fccfc675f951bacdfcd0261d8251b8a2f85e2da7
xcurx <suj5al78@gmail.com>
2025-03-04 13:42:29 +0530

    change timeline data

--- src/components/Timeline/timelineData.js
+++ src/components/Timeline/timelineData.js
@@ -1,7 +1,7 @@
 export const events = [
     {
       id: "01",
-      date: "15.01.2025",
+      date: "07.03.2025",
       title: "REGISTRATION BEGINS",
       description: "This marks the period when students can submit their applications to participate in CodeXCaliber. Ensure your application is complete before the deadline!"
     },
@@ -13,8 +13,8 @@ export const events = [
     // },
     {
       id: "02",
-      date: "25.01.2025",
-      title: "PROPOSAL WRITING",
+      date: "17.03.2025",
+      title: "PRODUCT IDEAS",
       description: "A week-long workshop focused on helping students write strong, well-structured project proposals. Get guidance from mentors and learn the best practices for proposing your project ideas."
     },
     // {
@@ -25,20 +25,14 @@ export const events = [
     // },
     {
       id: "03",
-      date: "15.02.2025",
+      date: "27.03.2025",
       title: "CODINGPHASE",
       description: "This is where the real fun begins! The core phase of CodeXCaliber, where students begin implementing their proposed projects. During this period, students will collaborate closely with mentors and contribute code to open-source repositories."
     },
     {
       id: "04",
-      date: "03.04.2025",
+      date: "27.05.2025",
       title: "MENTOR EVALUATION PHASE",
       description: "Mentors will review the progress of students’ projects, providing feedback and guidance. This evaluation is crucial for refining and improving the final submission."
-    },
-    {
-      id: "05",
-      date: "07.04.2025",
-      title: "CLOSING CEREMONY",
-      description: "The official conclusion of CodeXCaliber. Celebrate your achievements, review key learnings, and recognize outstanding contributors as we wrap up the program."
     },
   ]
